387ff041344b0c2df84157708e0adcac59965a8a
stephaniiema <54160019+stephaniiema@users.noreply.github.com>
2022-03-12 18:40:11 -0600

    suggestion/s working

--- lab09/src/Suggestions.js
+++ lab09/src/Suggestions.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Suggestion from './Suggestion.js';
+import {getHeaders} from './utils.js';
 
 class Suggestions extends React.Component {  
     constructor(props) {
         super(props);
+        this.state ={
+            suggestions: []
+        }
         // constructor logic
         console.log('Suggestions component created');
+        this.getSuggestions();
     }
 
     componentDidMount() {
@@ -12,11 +18,31 @@ class Suggestions extends React.Component {
         console.log('Suggestions component mounted');
     }
 
+    getSuggestions(){
+        fetch("/api/suggestions/", {
+        method: "GET",
+        headers: getHeaders()
+    })
+    .then(response => response.json())
+    .then(data => {
+        this.setState({
+            suggestions: data
+        })
+    });
+    }
+
     render () {
         return (
             <div className="suggestions">
                 <p className="suggestion-text">Suggestions for you</p>
-                <div>Suggestions...</div>
+                <div>
+                    {
+                        this.state.suggestions.map(suggestion => {
+                            return <Suggestion model={suggestion} key={'suggestion-' + suggestion.id} />
+                        })
+                    }
+                </div>
+
             </div>
         )     
     }
